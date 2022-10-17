4421935d82a7cf221b6b5fa95dd7bc46278c6910
Manny Khov <reangseik@gmail.com>
2022-10-17 15:21:38 +0700

    Add tracking to default page

--- resources/themes/default/views/products/add-to-cart.blade.php
+++ resources/themes/default/views/products/add-to-cart.blade.php
@@ -3,6 +3,21 @@
 <button
     type="submit"
     class="btn btn-lg btn-primary addtocart"
+    onclick="dataLayer.push({ 'ecommerce': null });dataLayer.push({
+        'event': 'add_to_cart',
+        'currency': 'USD',
+        'ecommerce': {
+                'items': [{
+                'item_id': '{{ $product->id }}',
+                'item_name': '{{ $product->name }}',
+                'item_brand': '',
+                'item_category': '',
+                'item_variant': '',
+                'currency': 'USD',
+                'price': '{{ $product->price }}'
+                }]
+            }
+        });"
     {{ ! $product->isSaleable() ? 'disabled' : '' }}
 >
     {{ $product->type == 'booking' ? __('shop::app.products.book-now') : __('shop::app.products.add-to-cart') }}
